36d29ac4754a6dfe7b347f71c40b0c55128db6d9
Md Wajid Ali <106772487+MdWajidAli@users.noreply.github.com>
2025-09-22 21:15:39 +0530

    adding the controller layer

--- src/main/java/com/calculator/project/Calculator/project/controller/CalculatorController.java
+++ src/main/java/com/calculator/project/Calculator/project/controller/CalculatorController.java
@@ -1,4 +1,53 @@
 package com.calculator.project.Calculator.project.controller;
 
+import com.calculator.project.Calculator.project.dto.OperationsResponse;
+import com.calculator.project.Calculator.project.service.CalculatorService;
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/api")
 public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    @Autowired
+    public CalculatorController(CalculatorService calculatorService){
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping("/add")
+    public OperationsResponse add(@RequestParam("a") BigDecimal a,
+                                  @RequestParam("b") BigDecimal b){
+        BigDecimal result = calculatorService.add(a,b);
+        return new OperationsResponse("add", a,b,result);
+    }
+
+    @GetMapping("/subtract")
+    public OperationsResponse subtract(@RequestParam("a") BigDecimal a,
+                                       @RequestParam("b") BigDecimal b){
+        BigDecimal result = calculatorService.subtract(a,b);
+        return new OperationsResponse("subtract", a,b,result);
+    }
+
+    @GetMapping("/multiply")
+    public OperationsResponse multiply(@RequestParam("a") BigDecimal a,
+                                       @RequestParam("b") BigDecimal b){
+        BigDecimal result = calculatorService.multiply(a,b);
+        return new OperationsResponse("multiply",a,b,result);
+    }
+
+    @GetMapping("/divide")
+    public OperationsResponse divide(@RequestParam("a") BigDecimal a,
+                                     @RequestParam("b") BigDecimal b)  {
+        BigDecimal result = calculatorService.divide(a,b);
+        return new OperationsResponse("divide", a,b,result);
+
+    }
+
 }
