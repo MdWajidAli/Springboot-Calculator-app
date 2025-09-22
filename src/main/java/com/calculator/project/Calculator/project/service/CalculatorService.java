a3ebf700694ffef44640db3520c6c411438e5273
Md Wajid Ali <106772487+MdWajidAli@users.noreply.github.com>
2025-09-22 20:43:57 +0530

    adding the service layer

--- src/main/java/com/calculator/project/Calculator/project/service/CalculatorService.java
+++ src/main/java/com/calculator/project/Calculator/project/service/CalculatorService.java
@@ -1,10 +1,12 @@
 package com.calculator.project.Calculator.project.service;
 
+import org.apache.coyote.BadRequestException;
+
 import java.math.BigDecimal;
 
 public interface CalculatorService {
     BigDecimal add(BigDecimal a, BigDecimal b);
     BigDecimal subtract(BigDecimal a, BigDecimal b);
     BigDecimal multiply(BigDecimal a,  BigDecimal b);
-    BigDecimal divide(BigDecimal a, BigDecimal b);
+    BigDecimal divide(BigDecimal a, BigDecimal b) throws BadRequestException;
 }
--- src/main/java/com/calculator/project/Calculator/project/service/implementations/CalculatorServiceImpl.java
+++ src/main/java/com/calculator/project/Calculator/project/service/implementations/CalculatorServiceImpl.java
@@ -1,4 +1,41 @@
 package com.calculator.project.Calculator.project.service.implementations;
 
-public class CalculatorServiceImpl {
+import com.calculator.project.Calculator.project.service.CalculatorService;
+import org.apache.coyote.BadRequestException;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+
+    int DIVIDE_SCALE = 10;
+
+    @Override
+    public BigDecimal add(BigDecimal a, BigDecimal b) {
+        return a.add(b);
+    }
+
+    @Override
+    public BigDecimal subtract(BigDecimal a, BigDecimal b) {
+        return a.subtract(b);
+    }
+
+    @Override
+    public BigDecimal multiply(BigDecimal a, BigDecimal b) {
+        return a.multiply(b);
+    }
+
+    @Override
+    public BigDecimal divide(BigDecimal a, BigDecimal b) throws BadRequestException {
+        if(b.compareTo(BigDecimal.ZERO) == 0){
+            throw new BadRequestException("Division by zero is not possible");
+        }
+        try {
+            return a.divide(b, DIVIDE_SCALE, RoundingMode.HALF_UP).stripTrailingZeros();
+        }catch (ArithmeticException ex){
+            return a.divide(b, DIVIDE_SCALE,RoundingMode.HALF_UP).stripTrailingZeros();
+        }
+    }
 }
